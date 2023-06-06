81c98ab9ea6d9a61847f74a3877585987724337a
samxalaliyev <samxalaliyev19@gmail.com>
2023-06-07 00:13:08 +0400

    adding mobile menu and 50% is completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../Logo--main.svg";
 import { FaSearch } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 import { IoLocationSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [activeLink, setActiveLink] = useState("AZ");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  const [sectionLink, setsectionLink] = useState("FS");
+
+  const Sections = (link) => {
+    setsectionLink(link);
+  };
+
   return (
     <div>
       <div className="header">
@@ -28,7 +47,7 @@ const Header = () => {
               </li>
             </ul>
             <ul className="mobile-ul">
-              <li>
+              <li className="birbank-icon">
                 <Link>
                   <img
                     alt="BirBank"
@@ -36,7 +55,7 @@ const Header = () => {
                   />
                 </Link>
               </li>
-              <li>
+              <li onClick={toggleMenu}>
                 <Link>
                   <svg
                     role="img"
@@ -57,6 +76,173 @@ const Header = () => {
             </ul>
           </nav>
         </div>
+      </div>
+      <div className={`mobile-nav${isOpen ? "-open" : ""}`}>
+        {isOpen && (
+          <div className="mobile-main">
+            <div className="mobile-menu-header">
+              <div className="container">
+                <div className="header-items">
+                  <ul className="language">
+                    <li>
+                      <Link
+                        to="#"
+                        className={activeLink === "AZ" ? "active" : ""}
+                        onClick={() => handleLinkClick("AZ")}
+                      >
+                        AZ
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="#"
+                        className={activeLink === "EN" ? "active" : ""}
+                        onClick={() => handleLinkClick("EN")}
+                      >
+                        EN
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="#"
+                        className={activeLink === "RU" ? "active" : ""}
+                        onClick={() => handleLinkClick("RU")}
+                      >
+                        RU
+                      </Link>
+                    </li>
+                  </ul>
+                  <div className="mobile-search"></div>
+                  <ul className="search-close">
+                    <li>
+                      <FaSearch color="#ef3e42" />
+                    </li>
+                    <li className="close" onClick={toggleMenu} id="close">
+                      <FiX color="#ef3e42" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="mobile-middle">
+              <ul>
+                <li>
+                  <Link
+                    to="/"
+                    className={sectionLink === "FS" ? "active" : ""}
+                    onClick={() => Sections("FS")}
+                  >
+                    Fiziki şəxslər üçün
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/"
+                    className={sectionLink === "BU" ? "active" : ""}
+                    onClick={() => Sections("BU")}
+                  >
+                    Biznes üçün
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mobile-content">
+              <ul className="accordion">
+                <li className="main-li">
+                  <input type="radio" name="accordion" id="kartlar" checked />
+                  <label htmlFor="kartlar">KARTLAR</label>
+                  <ul className="child-ul">
+                    <li>Bütün kartlar</li>
+                    <li>Taksit kartlar</li>
+                    <li>Debet kartlar</li>
+                    <li>Digital kart</li>
+                    <li>Taksit</li>
+                    <li>Hədiyyə kartı</li>
+                    <li>Kartın müddətinin uzadılması</li>
+                    <li>Kartlar üzrə xidmətlər</li>
+                    <li>Təhlükəsizlik qaydaları</li>
+                    <li>3D secure</li>
+                    <li>Kart sifarişi</li>
+                  </ul>
+                  <div className="card-img"></div>
+                </li>
+
+                <li className="main-li">
+                  <input type="radio" name="accordion" id="kreditler" checked />
+                  <label htmlFor="kreditler">KREDİTLƏR</label>
+                  <ul className="child-ul">
+                    <li style={{ color: "#ef3e42" }}>
+                      Çoxdan gözlənilən nağd kredit endirimi!
+                    </li>
+                    <li>Bütün kreditlər</li>
+                    <li>Nağd pul kediti</li>
+                    <li>Mikro Biznes Keditləri</li>
+                    <li>Təminatlı manat krediti</li>
+                    <li>Əmanət təminatlı kredit</li>
+                    <li>Avtomobil krediti</li>
+                    <li>Daşınmaz əmlak təminatlı kredit</li>
+                  </ul>
+                  <div className="card-img"></div>
+                </li>
+                <p>İPOTEKALAR</p>
+                <li className="main-li">
+                  <input
+                    type="radio"
+                    name="accordion"
+                    id="depozitler"
+                    checked
+                  />
+                  <label htmlFor="depozitler">DEPOZİTLƏR</label>
+                  <ul className="child-ul">
+                    <li>Bütün depozitlər</li>
+                    <li>Kapital depoziti</li>
+                    <li>Müddətsiz depozit</li>
+                    <li>Depozit seyfləri</li>
+                  </ul>
+                  <div className="card-img"></div>
+                </li>
+                <li className="main-li">
+                  <input
+                    type="radio"
+                    name="accordion"
+                    id="pulkocurmeleri"
+                    checked
+                  />
+                  <label htmlFor="pulkocurmeleri">PUL KÖÇÜRMƏLƏRİ</label>
+                  <ul className="child-ul">
+                    <li>Birbank ilə istənilən karta pul köçürün!</li>
+                    <li>Bütün pul köçürmələri</li>
+                    <li>Xəzri</li>
+                    <li>Zolotoya Korona</li>
+                    <li>Western Union</li>
+                  </ul>
+                  <div className="card-img"></div>
+                </li>
+                <li className="main-li">
+                  <input
+                    type="radio"
+                    name="accordion"
+                    id="onlaynxidmetler"
+                    checked
+                  />
+                  <label htmlFor="onlaynxidmetler">ONLAYN XİDMƏTLƏR</label>
+                  <ul className="child-ul">
+                    <li>Bütün online xidmətlər</li>
+                    <li>Kart siafrişi</li>
+                    <li>Kredot sifarişi</li>
+                    <li>Kredit ödənişi</li>
+                    <li>Kommunal ödənişləri</li>
+                    <li>Onlayn növbə</li>
+                    <li>Arayış və çıxarışların alınması</li>
+                  </ul>
+                  <div className="card-img"></div>
+                </li>
+                <p>KAMPANİYALAR</p>
+                <p>FƏRDİ BANKÇILIQ</p>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
